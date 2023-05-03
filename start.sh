c601d49b94186c8927c58f3c0fb2dd55a16980e6
Jay Lee <leeja744@student.otago.ac.nz>
2023-05-04 11:17:37 +1200

    added run file

---
+++ start.sh
@@ -0,0 +1,6 @@
+# when you first pull this, use this command in terminal:
+# `chmod +x start.sh`
+# this will allow you to run the whole app (back and front end) with the command
+# `./start.sh`
+
+gradlew bootRun & cd react-event-app && npm run start
