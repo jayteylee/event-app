dd68df41bbb9fde524f3955128bf3dadeccc0204
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-05-08 22:28:00 +1200

    add jwt auth to staff controller

--- src/main/java/org/example/controller/StaffController.java
+++ src/main/java/org/example/controller/StaffController.java
@@ -1,9 +1,13 @@
 package org.example.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.example.domain.Event;
 import org.example.service.event.EventService;
 import org.example.service.staff.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +23,29 @@ public class StaffController{
         this.staffService = staffService;
     }
 
-    @GetMapping("/login/{email}/{password}")
-    Boolean authenticate(@PathVariable("email") String email, @PathVariable("password") String password) {
-        return staffService.authenticate(email, password);
+//    @GetMapping("/login/{email}/{password}")
+//    Boolean authenticate(@PathVariable("email") String email, @PathVariable("password") String password) {
+//        return staffService.authenticate(email, password);
+//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        // Validate the username and password against your database or authentication provider
+        boolean isValidCredentials = staffService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
+
+        if (isValidCredentials) {
+            // Create a JWT using the user's username as the subject and a secret key
+            String jwt = Jwts.builder()
+                    .setSubject(loginRequest.getUsername())
+                    .signWith(Keys.hmacShaKeyFor("my-secret-key".getBytes()))
+                    .compact();
+
+            // Return the JWT in the response body
+            return ResponseEntity.ok(jwt);
+        } else {
+            // Return an unauthorized status code if the credentials are invalid
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
+
