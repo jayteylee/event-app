e1e23be90019b4231152444af68366c58dc7654e
Jay Lee <leeja744@student.otago.ac.nz>
2023-05-10 16:37:47 +1200

    removed create button from events, scholarships, research and liaisons if user is not staff

--- react-event-app/src/pages/Liasons/index.jsx
+++ react-event-app/src/pages/Liasons/index.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 function Liasons() {
     const [liasons, setLiasons] = useState([]);
+    const isStaff = sessionStorage.getItem('staff');
 
     useEffect(() => {
         loadLiasons();
@@ -44,7 +45,7 @@ function Liasons() {
                         <div className="flex flex-col w-1/5 h-full bg-slate-50 shadow-lg">
                             <h2 className="text-3xl font-bold text-center mt-8">Welcome back!</h2>
                             <div className="flex flex-row justify-center h-10 my-7">
-                                <button value="create-liason" type="button" onClick={handleClick} className="hover:bg-slate-100 transition-all rounded-md justify-center w-3/5 border shadow-md font-poppins text-black font-semibold mx-2">Create Liason</button>
+                                {isStaff && <button value="create-liason" type="button" onClick={handleClick} className="hover:bg-slate-100 transition-all rounded-md justify-center w-3/5 border shadow-md font-poppins text-black font-semibold mx-2">Create Liason</button>}
                             </div>
                         </div>
                         <div className="flex flex-col w-4/5 h-full">
