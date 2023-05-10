0ef9f73256b09486cb6e2e86b246def975e3a0f4
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-05-10 17:14:11 +1200

    generate UUID passwords for students

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,11 +21,13 @@ INSERT INTO Scholarship (scholarship_id, name, num_available, subject, type) VAL
 INSERT INTO Research_opportunity (opportunity_id, title, type, subject) VALUES (111, 'AI Research Internship', 'Internship', 'Artificial Intelligence');
 INSERT INTO Research_opportunity (opportunity_id, title, type, subject) VALUES (101, 'Database Optimisation Summer Research Project', 'Research Project', 'Databases');
 
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (2938475, 'LKAdmin', 'kitla230@student.otago.ac.nz', 'apple123', '2000-06-24');
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (56473829, 'JLAdmin', 'leeja744@student.otago.ac.nz', '321', '1942-1-4');
 
-INSERT INTO Student (student_id, name, email, password, dob) VALUES (123456789, 'Lachy Kitchen', 'kitla230@student.otago.ac.nz', 'apple123', '2000-06-24');
-INSERT INTO Student (student_id, name, email, password, dob) VALUES (987654321, 'Jay Lee', 'leeja744@student.otago.ac.nz', '321', '1942-1-4');
-INSERT INTO Student (student_id, name, email, password, dob) VALUES (111111, 'Call', 'cal', '987', '1989-09-02');
-INSERT INTO Student (student_id, name, email, password, dob) VALUES (1, 'Mahfuz', 'fuz', '420', '2010-10-10');
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (123456789, 'Lachy Kitchen', 'kitla23@gmail.com', '9e1fee7c-d594-4f26-99ff-10465e1b36d5', '2000-06-24');
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (987654321, 'Jay Lee', 'leeja123@gmail.com', '0cb3442b-dcc6-41a1-9203-b3940eead052', '1942-1-4');
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (111111, 'Call', 'cal@gmail.com', '27392bdc-8d84-4416-a55a-115ea88d9194', '1989-09-02');
+INSERT INTO Student (student_id, name, email, password, dob) VALUES (1, 'Mahfuz', 'fuz@gmail.com', '010f2042-af58-4626-ba5d-3ad53e826b51', '2010-10-10');
 
 INSERT INTO Staff (staff_id, email, first_name, last_name, password, phone_number) VALUES (3976743, 'leeja744@staff.otago.ac.nz', 'Jay', 'Lee', '123', '0211234567');
 INSERT INTO Staff (staff_id, email, first_name, last_name, password, phone_number) VALUES (2976743, 'cal@staff.otago.ac.nz', 'Cal', 'Keenan', '123', '0271234567');
